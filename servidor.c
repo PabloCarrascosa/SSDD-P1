b89fe338ee2d3095e8da4a0d00d2b0df0ef42354
PabloCarrascosa <100429084@alumnos.uc3m.es>
2023-03-11 15:15:13 +0100

    implementadas funcionalidades init, set_value, modify_value, delete_key y exist

--- servidor.c
+++ servidor.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <sys/stat.h>
 #include "claves.h"
 
@@ -12,6 +13,142 @@ int request_not_copied = true;
 pthread_cond_t request_cond;
 mqd_t server_q;
 struct mq_attr attr;
+
+int init(){
+	DIR* dir = opendir("key_db");
+	if (dir){
+		struct dirent *entry;
+		while((entry = readdir(dir))!= 0){     
+			if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+				char path[1024];
+				sprintf(path,"key_db/%s", entry->d_name);
+				unlink(path);
+			}
+		}
+	}
+	else{
+		if(mkdir("key_db", 0755)<0){ // o 0777
+				closedir(dir);
+				return -1;
+			}
+		}
+		closedir(dir);
+		return 0;
+}
+
+int set_value(int key, char *value1, int value2, double value3){
+	DIR* dir = opendir("key_db");
+	FILE* keydata;
+	char path[1024];
+	char full_path[2048];
+	sprintf(path,"%d.txt", key);
+	sprintf(full_path, "key_db/%s", path);
+
+	if (dir){
+		struct dirent *entry;
+		while((entry = readdir(dir))!= 0){
+			if (strcmp(entry->d_name, path) == 0){
+				closedir(dir);
+				return -1;
+			}
+		}
+		keydata = fopen(full_path, "w");
+		fprintf(keydata, "%d;%s;%d;%f",
+		key, 
+		value1, 
+		value2, 
+		value3);
+		if (fclose(keydata) < 0){
+			closedir(dir);
+			return -1;
+		}
+		closedir(dir);
+		return 0;
+	}
+	else{
+		return -1;
+	}
+}
+/*
+int get_value(int key, char *value1, int *value2, double *value3){
+
+}
+*/
+int modify_value(int key, char *value1, int value2, double value3){
+	DIR* dir = opendir("key_db");
+	FILE* keydata;
+	char path[1024];
+	char full_path[2048];
+	sprintf(path, "%d.txt", key);
+	sprintf(full_path, "key_db/%s", path);
+	if (dir){
+		struct dirent *entry;
+		while((entry = readdir(dir))!= 0){
+			if (strcmp(entry->d_name, path) == 0){
+				keydata = fopen(full_path, "w");
+				fprintf(keydata, "%d;%s;%d;%f",
+				key, 
+				value1, 
+				value2, 
+				value3);
+				if (fclose(keydata) < 0){
+					closedir(dir);
+					return -1;
+				}
+				closedir(dir);
+				return 0;
+			}
+		}
+		closedir(dir);
+		return -1;
+	}
+	else{
+		return -1;
+	}
+}
+
+int delete_key(int key){
+	DIR* dir = opendir("key_db");
+	char path[1024];
+	char full_path[2048];
+	sprintf(path, "%d.txt", key);
+	sprintf(full_path, "key_db/%s", path);
+	if (dir){
+		struct dirent *entry;
+		while((entry = readdir(dir))!= 0){
+			if (strcmp(entry->d_name, path) == 0){
+				
+				unlink(full_path);
+				closedir(dir);
+				return 0;
+			}
+		}
+		closedir(dir);
+		return -1;
+	}
+	else{
+		return -1;
+	}
+}
+int exist(int key){
+	DIR* dir = opendir("key_db");
+	char path[1024];
+	sprintf(path, "%d.txt", key);
+	if (dir){
+		struct dirent *entry;
+		while((entry = readdir(dir))!= 0){
+			if (strcmp(entry->d_name, path) == 0){
+				closedir(dir);
+				return 1;
+			}
+		}
+		closedir(dir);
+		return -1;
+	}
+	else{
+		return -1;
+	}
+}
 
 /* Función llamada para tratar las peticiones recibidas */
 void tratar_peticion(void *req){
@@ -27,24 +164,36 @@ void tratar_peticion(void *req){
 
 	switch(request.op){
 		case INIT:
-		// INICIALIZA EL ENTORNO PARA ALMACENAR LAS TUPLAS
-		struct stat st = {0};
-		if (stat("/key_db", &st) == -1) {
-			mkdir("/some/directory", 0700);
-		}
-		case SET_VALUE:
-		// LE DA VALORES VALORES A LA TUPLA
-		case GET_VALUE:
-		// OBTIENE VALORES DE TUPLA
-		case MODIFY_VALUE:
-		// CAMBIA VALORES DE TUPLA
-		case DELETE_KEY:
-		// ELIMINA LA TUPLA
-		case EXIST:
-		// COMPRUEBA LA TUPLA
-		
+		/* FUNCIONALIDAD INIT */
+			init();
+			break;
 
+		case SET_VALUE:
+		/* FUNCIONALIDAD SET_VALUE */
+			response.status = set_value(request.data.key, request.data.value1, request.data.value2, request.data.value3);
+			break;
+			
+		/* FUNCIONALIDAD GET_VALUE */
+		case GET_VALUE:
+
+		/* FUNCIONALIDAD MODIFY_VALUE */
+		case MODIFY_VALUE:
+			response.status = modify_value(request.data.key, request.data.value1, request.data.value2, request.data.value3);
+			break;
+
+		/* FUNCIONALIDAD DELETE_KEY */
+		case DELETE_KEY:
+			response.status = delete_key(request.data.key);
+			break;
+
+		/* FUNCIONALIDAD EXIST */
+		case EXIST:
+			response.status = exist(request.data.key);
+			break;
+
+			
 	}
+		
 	client_q = mq_open(request.name, O_WRONLY);
 	if (client_q < 0){
 		printf("Error al abrir la cola del cliente\n");
@@ -62,11 +211,9 @@ void tratar_peticion(void *req){
 	pthread_exit(0);
 
 }
-
 /* Abre la cola del servidor, recibe las peticiones, y espera a que se traten para mandar la respuesta */
 int main(void){
 	Request req;
-	Response res;
 	pthread_t thid;
 	pthread_attr_t t_attr;
 
